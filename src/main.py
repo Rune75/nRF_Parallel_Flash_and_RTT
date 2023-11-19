1355b7f023588c0af6c10daf0214704f5f6ab834
Rune Bekkevold <rune.bekkevold@gmail.com>
2023-11-19 17:09:50 +0100

    reset

---
+++ src/main.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+import multiprocessing 
+import pylink
+import FlashAndprovision as fp #Import Flash and config module
+
+#hexFile='./nRF_hexFiles/rttShell840.hex'
+hexFile='./nRF_hexFiles/rttShell833.hex'
+
+def flashProcess(serialnumbers):
+    fp.FLasAndConfig(serialnumbers,hexFile)
+
+#------------------------------------------
+#-- Main 
+#------------------------------------------
+
+#-- Get a list of connected JLinks serial numbers to run in parallel.
+jlinkSerialNumbers = fp.getJLinkSerials(pylink.JLink())    
+print("SerialNr: ", jlinkSerialNumbers)
+
+
+#- Execute Jlink programming and RTT configuration in parallel 
+with multiprocessing.Pool() as pool: 
+    pool.map(flashProcess, jlinkSerialNumbers)
+
+print("Finished paralell programming.")
+
+
